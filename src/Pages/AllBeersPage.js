b9bfa988e964a8ad6c87d2ac75c9373a761fe144
Henrique Mazzoleni <henrique.mazzoleni@gmail.com>
2023-03-10 13:26:07 -0300

    iteration 7 completed

--- src/Pages/AllBeersPage.js
+++ src/Pages/AllBeersPage.js
@@ -1,14 +1,20 @@
 import { Fragment, useEffect, useState } from "react"
 import axios from "axios";
 import BeerShortCard from "../components/BeerShortCard";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 
 export default function AllBeersPage () {
   const [beersList, setBeersList] = useState([]);
+  const [query, setQuery] = useState('')
+
+  
+  const queryHandler = (event) => {
+    setQuery(event.target.value)
+  }
 
   useEffect(() => {
     const fetchBeers = async () => {
-      const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers")
+      const response = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`)
       setBeersList(response.data)
     }
     try {
@@ -16,12 +22,14 @@ export default function AllBeersPage () {
     } catch (error) {
       console.log(error);
     }
-  },[])
+  },[query])
 
     return (
       <Fragment>
         <Navbar />
-        <h1>All Beers</h1>
+        <label>Search Beers
+          <input type="text" value={query} onChange={queryHandler} />
+        </label>
         <ul>
           {beersList.length !== 0 &&
             beersList.map((beer) => <BeerShortCard key={beer._id} {...beer} />)}
